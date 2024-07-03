ab0457b26a0d9109c841121cc98415150dcbda29
JTG947 <johnnythegreat947@gmail.com>
2024-07-03 17:45:52 -0400

    home screen basic UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,58 @@
 import { View, Text } from '../../components/Themed';
+import { StyleSheet } from 'react-native';
 import React, { FunctionComponent } from 'react';
+import { Image } from 'react-native';
+import { green } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+
 interface HomeScreenProps {
     
 }
  
-const HomeScreen: FunctionComponent<HomeScreenProps> = () => {
+const HomeScreen = () => {
     return (
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>HomeScreen Component</Text>
+        <View style={styles.root}>
+            <Image style= {styles.image}source={require('../../assets/images/Saly-1.png')} />
+            <Text style= {styles.header}>HOPE TO SAVE THE EXISTANCE - BFH  </Text>
+            <Text style= {styles.header2}> BFH is a movement for the sake of HOPE </Text>
         </View>
      );
 }
  
 export default HomeScreen;
+
+
+const styles = StyleSheet.create({
+    root:{
+        padding: 20,
+        alignItems: 'center',
+        flex: 1,
+    },
+    image: {
+        height: '60%',
+        aspectRatio: 1,
+        backgroundColor: 'black',
+        width:'30%',
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header : {
+      fontSize: 30,
+      fontWeight: 'bold',
+      marginTop: 50,
+      marginBottom: 15,
+    },
+    header2 : {
+      fontSize: 20,
+      textAlign: 'center',
+      color: '#707070' ,
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: '80%',
+    },
+
+  });
